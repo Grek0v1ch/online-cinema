60c5e9902100cf5a080945f2e228bdfd75517b8a
Ivan Voronov <ivvoronov2308@gmail.com>
2024-05-27 11:42:36 +0300

    Translate the login page and add some changes

--- cinema-app/app/forms.py
+++ cinema-app/app/forms.py
@@ -3,10 +3,9 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField, Selec
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
+    username = StringField('Логин', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    submit = SubmitField('Вход')
 
 
 class PastebinEntry(FlaskForm):
